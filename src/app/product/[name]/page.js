32f47d4011d63849b7de9a760202b07881175ec2
Mohammad Sahragard <mohammadsah4481@gmail.com>
2023-11-18 00:56:47 +0330

    create 'product' route and ready to  create other components.

---
+++ src/app/product/[name]/page.js
@@ -0,0 +1,15 @@
+const Product = ({ params }) => {
+
+    const productName = params.name;
+
+
+    return (
+        <div className='relative min-h-screen bg-white'>
+            <div className='p-[100px_3%_0_3%] lg:p-[100px_150px_0_150px] space-y-10'>
+            </div>
+        </div>
+    );
+};
+
+
+export default Product;
